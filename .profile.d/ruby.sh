7b73feacf1898f33baff2df2c03ecac222680888
root <root@openproject>
2018-09-09 19:49:11 +0300

    Merge confilct resolved

---
+++ .profile.d/ruby.sh
@@ -0,0 +1,8 @@
+export LANG=${LANG:-en_US.UTF-8}
+#export GEM_PATH="$HOME/vendor/bundle/ruby/2.4.0:$GEM_PATH"
+export PATH="/home/openproject/.rbenv/shims:$PATH"
+export RAILS_ENV=${RAILS_ENV:-production}
+export RACK_ENV=${RACK_ENV:-production}
+export SECRET_KEY_BASE=${SECRET_KEY_BASE:-0e1e5fd95a2b1c3baab68272da2c49dda4de3f15363e9d25797e3c964b36eb04e9be75bd8c9c751d9ea83b55a9624762ee8f2c509cdb40afd81ab334a5f38cfa}
+export RAILS_SERVE_STATIC_FILES=${RAILS_SERVE_STATIC_FILES:-enabled}
+export RAILS_LOG_TO_STDOUT=${RAILS_LOG_TO_STDOUT:-enabled}
